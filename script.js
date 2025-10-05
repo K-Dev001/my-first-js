b130aef6f623662a9724b89a096cc9ba911b7d7c
Kyota <kyota.work@gmail.com>
2025-10-05 23:25:26 +0900

    feat: カウンター実装

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// document.getElementById("btn").addEventListener("click", () => {
+//   document.getElementById("out").textContent = "Hello, JS!";
+//   console.log("ボタンが押されました");
+// });
+
+let n = 0; 
+const $count = document.getElementById("count"); 
 document.getElementById("btn").addEventListener("click", () => {
-  document.getElementById("out").textContent = "Hello, JS!";
-  console.log("ボタンが押されました");
-});
+    n += 1; 
+    $count.textContent = n;
+    if (n === 10) alert("到達！"); 
+}); 
