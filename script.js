fecf90d1871b9eab54d99b82bd09095ba75ceeb9
Kyota <kyota.work@gmail.com>
2025-10-06 01:43:22 +0900

    入力フォーム

--- script.js
+++ script.js
@@ -3,10 +3,26 @@
 //   console.log("ボタンが押されました");
 // });
 
-let n = 0; 
-const $count = document.getElementById("count"); 
+// カウンター
+// let n = 0; 
+// const $count = document.getElementById("count"); 
+// document.getElementById("btn").addEventListener("click", () => {
+//     n += 1; 
+//     $count.textContent = n;
+//     if (n === 10) alert("到達！"); 
+// }); 
+
+
+// 入力フォーム
+const $name = document.getElementById("name"); 
+const $out = document.getElementById("out"); 
+
 document.getElementById("btn").addEventListener("click", () => {
-    n += 1; 
-    $count.textContent = n;
-    if (n === 10) alert("到達！"); 
+    if($name.value.trim() === ""){
+        $out.textContent = "名前を入力してください"; 
+        $out.style.color = "red"; 
+    } else {
+        $out.textContent = `こんにちは、${$name.value}さん！`; 
+        $out.style.color = "black"; 
+    }
 }); 
